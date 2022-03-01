35d46729f56c969f3d33706d77d5a3569e4dcd58
shestyf <sofia.styf@masarinconsulting.se>
2022-03-01 16:30:11 +0100

    fixed tests for handler

--- internal/pkg/presentation/handler_test.go
+++ internal/pkg/presentation/handler_test.go
@@ -7,7 +7,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/diwise/integration-incident/internal/pkg/infrastructure/repositories/models"
+	"github.com/diwise/integration-incident/internal/pkg/application"
 	"github.com/matryer/is"
 )
 
@@ -15,48 +15,51 @@ func TestNotificationHandlerDoesNothingIfDeviceStateDoesNotExist(t *testing.T) {
 	is := is.New(t)
 
 	server := setupMockService(http.StatusOK, "")
+	app := mockApp()
 
 	r := httptest.NewRequest("POST", server.URL+"/notification", bytes.NewBuffer([]byte(noDeviceState)))
 	w := httptest.NewRecorder()
 
-	reporter := newIncidentReporterThatReturns(nil)
-
-	notificationHandler(reporter.f).ServeHTTP(w, r)
+	notificationHandler(app).ServeHTTP(w, r)
 	is.Equal(w.Code, http.StatusOK)
+
+	is.Equal(len(app.DeviceStateUpdatedCalls()), 0)
 }
 
 func TestNotificationHandlerAddsNewDeviceStateIfNoPreviousStateExists(t *testing.T) {
 	is := is.New(t)
 
 	server := setupMockService(http.StatusOK, "")
+	app := mockApp()
 
 	r := httptest.NewRequest("POST", server.URL+"/notification", bytes.NewBuffer([]byte(createStatusBody("123", "104"))))
 	w := httptest.NewRecorder()
 
-	reporter := newIncidentReporterThatReturns(nil)
-
-	notificationHandler(reporter.f).ServeHTTP(w, r)
+	notificationHandler(app).ServeHTTP(w, r)
 	is.Equal(w.Code, http.StatusOK)
+
+	is.Equal(len(app.DeviceStateUpdatedCalls()), 1)
 
 	//this test doesn't actually check that a new device state has been stored yet
 }
 
-func TestNotificationHandlerSendsNewIncidentReportIfDeviceStateHasChanged(t *testing.T) {
+func TestNotificationHandlerTriggersDeviceStateUpdatedIfDeviceStateHasChanged(t *testing.T) {
 	is := is.New(t)
 
 	server := setupMockService(http.StatusOK, "")
+	app := mockApp()
 
 	r := httptest.NewRequest("POST", server.URL+"/notification", bytes.NewBuffer([]byte(createStatusBody("123", "104"))))
 	w := httptest.NewRecorder()
 
-	reporter := newIncidentReporterThatReturns(nil)
-
-	notificationHandler(reporter.f).ServeHTTP(w, r)
+	notificationHandler(app).ServeHTTP(w, r)
 	is.Equal(w.Code, http.StatusOK)
 
 	r2 := httptest.NewRequest("POST", server.URL+"/notification", bytes.NewBuffer([]byte(createStatusBody("123", "205"))))
 
-	notificationHandler(reporter.f).ServeHTTP(w, r2)
+	notificationHandler(app).ServeHTTP(w, r2)
+	is.Equal(w.Code, http.StatusOK)
+	is.Equal(len(app.DeviceStateUpdatedCalls()), 2)
 }
 
 func setupMockService(statusCode int, body string) *httptest.Server {
@@ -71,40 +74,13 @@ const noDeviceState string = `{"subscriptionId":"36990e41ccd84af99d8b233eca81d1d
 const withDeviceStateJsonFormat string = `{"subscriptionId":"36990e41ccd84af99d8b233eca81d1d3","data":[{"id":"urn:ngsi-ld:Device:se:servanet:lora:msva:%s","type":"Device","rssi":{"type":"Property","value":0.1},"snr":{"type":"Property","value":0.41},"deviceState":{"type":"Property","value":"%s"}}]}`
 
 func createStatusBody(deviceId, state string) string {
-
 	return fmt.Sprintf(withDeviceStateJsonFormat, deviceId, state)
 }
 
-func newIncidentReporterThatReturns(err error) *incidentReporter {
-	return &incidentReporter{returnValue: err}
-}
-
-type incidentReporter struct {
-	callCount   int32
-	returnValue error
-}
-
-func (r *incidentReporter) assertCallCount(t *testing.T, expected int32) {
-	if r.callCount != expected {
-		if expected == 0 {
-			t.Errorf("Incident reporter should not have been called, but was called %d times!", r.callCount)
-		} else if expected == 1 {
-			t.Errorf("Incident reporter should have been called once, but was called %d times!", r.callCount)
-		} else {
-			t.Errorf("Incident reporter should have been called %d times, but was called %d times!", expected, r.callCount)
-		}
+func mockApp() *application.IntegrationIncidentMock {
+	return &application.IntegrationIncidentMock{
+		DeviceStateUpdatedFunc: func(deviceId, deviceState string) error {
+			return nil
+		},
 	}
-}
-
-func (r *incidentReporter) assertCalledOnce(t *testing.T) {
-	r.assertCallCount(t, 1)
-}
-
-func (r *incidentReporter) assertNotCalled(t *testing.T) {
-	r.assertCallCount(t, 0)
-}
-
-func (r *incidentReporter) f(models.Incident) error {
-	r.callCount++
-	return r.returnValue
 }
